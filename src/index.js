e12eeb65cea601e6310bf9c64b8c0bc59e4a812a
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-21 22:23:03 -0400

    I have redux hooking up properly right now to pass in the details to the header component using state from the reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { reducer as detailsReducer } from "./reducers/details";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootReducer = combineReducers({
+  details: detailsReducer
+});
+const store = createStore(rootReducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
